0dd77cd1b084c523a1cb8191cee0f526b27ee677
Igor Fernandes <33417759+ifmf@users.noreply.github.com>
2020-01-27 17:43:42 +0000

    create Init-page
    Criação do init-page para reorganizar os componentes, alguns dos componentes foram eliminados e passaram a integrar este

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Tsi19Component } from './tsi19/tsi19.component';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    redirectTo: '/'
-  },
-  {
-    path: 'TSI2Market19',
-    component: Tsi19Component
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
